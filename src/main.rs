e9a5714988428fbf4a45ada217ac5bbe5a421fca
mkybdev <mitukouyb@gmail.com>
2024-10-28 14:48:24 +0900

    added clap crate, implemented cli interface

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use clap::Parser;
+use std::path::Path;
+
+/// .gitignore file refactoring tool
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the .gitignore file to refactor
+    #[arg(
+        short,
+        long,
+        help = "Path to the .gitignore file to refactor (If not provided, the tool will look for a .gitignore file in the current directory)"
+    )]
+    path: Option<String>,
+
+    /// Refactoring level (1-3)
+    #[arg(short, long, allow_hyphen_values = true, default_value_t = 2)]
+    level: i8,
+}
+
+fn validate_args(args: &Args) -> (&Path, u8) {
+    let path = Path::new(args.path.as_deref().unwrap_or("./.gitignore"));
+    if let Some(_) = &args.path {
+        if !path.exists() {
+            eprintln!("Error: The provided path does not exist.");
+            std::process::exit(1);
+        }
+    } else if !path.exists() {
+        eprintln!("Error: No .gitignore file found in the current directory.");
+        std::process::exit(1);
+    }
+
+    if args.level > 3 || args.level <= 0 {
+        eprintln!(
+            "Error: Invalid refactoring level. The refactoring level must be between 1 and 3."
+        );
+        std::process::exit(1);
+    }
+
+    (path, args.level as u8)
+}
+
+fn main() {
+    let args = Args::parse();
+    let (path, level) = validate_args(&args);
+    println!("Path: {:?}, Level: {}", path, level);
+}
