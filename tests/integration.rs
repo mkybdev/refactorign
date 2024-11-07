5a468b34b62bd348875ee66359e0cd91b12945da
mkybdev <mitukouyb@gmail.com>
2024-11-08 00:39:59 +0900

    deleted integration test code (for future work)

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,22 +1,4 @@
-use walkdir::WalkDir;
-
 extern crate refactorign;
 
 #[test]
-fn integration_test() {
-    for entry in WalkDir::new("tests/data")
-        .into_iter()
-        .filter_map(Result::ok)
-    {
-        let path = entry.path();
-        if path.ends_with("gitignore") {
-            for level in 1..=1 {
-                println!("\r\n{:?}: ------------------------\r\n", path);
-                let result = refactorign::Refactor::run_verbose(&path, level);
-                println!("\r\nResult: ------------------------\r\n");
-                result.file().print();
-                println!("\r\n------------------------\r\n");
-            }
-        }
-    }
-}
+fn integration_test() {}
