467de85e15b895ea928684ea9d3b4e8583422f3e
mkybdev <mitukouyb@gmail.com>
2024-11-07 12:23:00 +0900

    introduced RefCell to make State.file mutable, implemented some methods for Refactor

--- src/process/refactor.rs
+++ src/process/refactor.rs
@@ -1,37 +1,74 @@
-use crate::core;
+use crate::core::{file::File, tree::DirectoryTree};
+use std::cell::{Ref, RefCell};
+use std::ops::Deref;
 use std::path::Path;
 
+#[derive(Clone)]
 pub struct State {
-    pub file: core::file::File,
+    file: RefCell<File>,
     pub level: u8,
-    pub tree: core::tree::DirectoryTree,
+    pub tree: DirectoryTree,
 }
+
+struct StateFileValue<'a> {
+    value: Ref<'a, File>,
+}
+
+impl StateFileValue<'_> {
+    pub fn get(&self) -> &File {
+        &self.value
+    }
+}
+
+// impl<'b> Deref for StateFileValue<'b> {
+//     type Target = File;
+//     fn deref(&self) -> &File {
+//         &self.value
+//     }
+// }
+
 impl State {
-    pub fn new(path: &Path) -> Self {
+    pub fn new(path: &Path, level: u8) -> Self {
         State {
-            file: core::file::File::new(path.to_path_buf()),
-            level: 0,
-            tree: core::tree::DirectoryTree::new(),
+            file: RefCell::new(File::new(path.to_path_buf())),
+            level,
+            tree: DirectoryTree::new(),
         }
     }
 }
 
+#[derive(Clone)]
 pub struct Refactor {
     pub state: State,
 }
 impl Refactor {
-    pub fn new(path: &Path) -> Self {
+    pub fn new(path: &Path, level: u8) -> Self {
         Refactor {
-            state: State::new(path),
+            state: State::new(path, level),
         }
     }
-    pub fn run(path: &Path, level: u8) {
-        let refactor = &mut Refactor::new(path);
-        let (file, tree) = refactor.basic_process();
-        let refactor = Refactor {
-            state: State { file, level, tree },
+    pub fn file(&self) -> File {
+        let value = StateFileValue {
+            value: self.state.file.borrow(),
         };
-        let result = refactor.re_include().containment().merge();
-        result.state.file.print();
+        value.get().clone()
+    }
+    pub fn file_mut(&mut self) -> &mut File {
+        self.state.file.get_mut()
+    }
+    pub fn tree(&self) -> &DirectoryTree {
+        &self.state.tree
+    }
+    pub fn tree_mut(&mut self) -> &mut DirectoryTree {
+        &mut self.state.tree
+    }
+    pub fn run(path: &Path, level: u8) -> Refactor {
+        let refactor = &mut Refactor::new(path, level);
+        refactor
+            .basic_process()
+            .re_include()
+            .containment()
+            .merge()
+            .clone()
     }
 }
