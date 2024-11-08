22505e7bea425cc7c54d316e3789cfce0e97438d
mkybdev <mitukouyb@gmail.com>
2024-11-08 23:41:13 +0900

    rewrote re_include to pass test 3

--- src/process/re_include.rs
+++ src/process/re_include.rs
@@ -3,14 +3,20 @@ use std::{fs, path::PathBuf};
 use fs_tree::FsTree;
 
 use super::refactor::Refactor;
+use crate::printv;
 
-fn get_children(globals: Vec<PathBuf>, parent_path: PathBuf) -> Vec<PathBuf> {
+fn get_children(
+    globals: Vec<PathBuf>,
+    root_path: PathBuf,
+    parent_path: PathBuf,
+    gign_path: PathBuf,
+) -> Vec<PathBuf> {
     // get children (except globally ignored ones)
-    if let Ok(rd) = fs::read_dir(parent_path) {
+    if let Ok(rd) = fs::read_dir(root_path.join(parent_path)) {
         rd.filter_map(|entry| {
             let path = entry.unwrap().path();
-            if !globals.contains(&path) {
-                Some(path)
+            if !globals.contains(&PathBuf::from(path.file_name().unwrap())) && path != gign_path {
+                Some(path.strip_prefix(root_path.clone()).unwrap().to_path_buf())
             } else {
                 None
             }
@@ -27,22 +33,39 @@ impl Refactor {
         let root = self.root().clone();
         let tree = self.tree().clone();
         let file = self.file_mut();
+        if verbose {
+            printv!(root, tree);
+        }
         // iterate over nodes (parent nodes)
         // parent nodes should not be ignored for re-including children
-        for parent_path in FsTree::read_at(&root).unwrap().paths().skip(0) {
+        for parent_path in FsTree::read_at(&root).unwrap().paths() {
             if let Some(parent) = tree.root.get(parent_path.clone()) {
                 // check if parent is not ignored
                 if tree.node_line_map.get(&parent_path).is_none() {
-                    if let Some(ign_children) = parent.children() {
-                        let ign_children_num = ign_children.len(); // number of ignored children
-                        let children =
-                            get_children(tree.globals.clone(), root.clone().join(&parent_path));
-                        let children_num = children.len(); // number of children (except globally ignored ones)
+                    if let Some(ign_children_map) = parent.children() {
+                        // ignored children (should be in node_line_map)
+                        let ign_children = ign_children_map
+                            .keys()
+                            .filter(|&path| {
+                                tree.node_line_map.contains_key(&parent_path.join(path))
+                            })
+                            .map(|key| parent_path.join(key))
+                            .collect::<Vec<PathBuf>>();
+                        let ign_children_num = ign_children.len();
+                        // all children (except globally ignored ones)
+                        let children = get_children(
+                            tree.globals.clone(),
+                            root.clone(),
+                            parent_path.clone(),
+                            file.path.clone(),
+                        );
+                        let children_num = children.len();
+                        if verbose {
+                            printv!(parent_path, ign_children, children);
+                        }
                         if ign_children_num > (children_num + 1) / 2 {
                             // remove lines
-                            for child_path in
-                                ign_children.keys().map(|child| parent_path.join(child))
-                            {
+                            for child_path in ign_children.clone().into_iter() {
                                 file.remove_line_with_path(child_path, verbose);
                             }
                             // ignore parent
@@ -51,13 +74,8 @@ impl Refactor {
                                 verbose,
                             );
                             // re-include child(ren) not ignored
-                            for child in children {
-                                let child_path = child.strip_prefix(&root).unwrap();
-                                if ign_children
-                                    .keys()
-                                    .find(|&path| parent_path.join(path) == child_path)
-                                    .is_none()
-                                {
+                            for child_path in children {
+                                if !ign_children.contains(&child_path) {
                                     let new_line = format!("!{}", child_path.to_str().unwrap());
                                     file.add_line(new_line, verbose);
                                 }
@@ -78,15 +96,15 @@ mod tests {
     #[test]
     fn test_re_include() {
         for path in test::get_input_paths("re_include") {
-            for level in 1..=3 {
+            for level in 1..=1 {
                 test::show_title(&path, level);
                 let refactor = &mut Refactor::new(&path, level, true);
                 let result = refactor.basic_process().re_include();
+                test::show_result(&result.file());
                 assert!(test::file_cmp(
                     result.file(),
                     test::get_expected_path(&path)
                 ));
-                test::show_result(&result.file());
             }
         }
     }
