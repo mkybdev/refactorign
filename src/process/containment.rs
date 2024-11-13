eeee3b790c3cdde4eafc2d478b70ec82286ae0ba
mkybdev <mitukouyb@gmail.com>
2024-11-13 10:40:48 +0900

    finished implementing containment process

--- src/process/containment.rs
+++ src/process/containment.rs
@@ -9,20 +9,32 @@ impl Refactor {
         let verbose = self.verbose().clone();
         let root = self.root().clone();
         let tree = self.tree().clone();
-        let file = self.file_mut();
+        // let file = self.file_mut();
         if verbose {
             printv!(root, tree);
         }
+        // directory-structural containment
         for parent in tree.root.paths().skip(1) {
             let children = tree.root.get(&parent).unwrap().children().unwrap();
-            for child in children.keys() {
-                if let Some(line) = tree.node_line_map.get(&parent.join(child.clone())) {
+            // global containment (wildcard / global)
+            if self.is_globally_ignored(&parent) {
+                if let Some(line) = tree.node_line_map.get(&parent) {
+                    let file = self.file_mut();
                     file.remove_line_with_path(PathBuf::from(line.content.unwrap()), verbose);
                 }
             }
+            // normal containment
+            if self.is_normally_ignored(&parent) {
+                for child in children.keys() {
+                    if let Some(line) = tree.node_line_map.get(&parent.join(child.clone())) {
+                        let file = self.file_mut();
+                        file.remove_line_with_path(PathBuf::from(line.content.unwrap()), verbose);
+                    }
+                }
+            }
         }
-        // rebuild tree
-        self.rebuild_tree();
+        // update state
+        self.update();
         self
     }
 }
