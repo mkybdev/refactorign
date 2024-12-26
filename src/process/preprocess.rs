02bb9ce6a8897e6bde0b5a78da3e524e84136b55
mkybdev <mitukouyb@gmail.com>
2024-12-26 10:55:58 +0900

    changed operation execution order

--- src/process/preprocess.rs
+++ src/process/preprocess.rs
@@ -3,18 +3,14 @@ use crate::{printv, tree::DirectoryTree, Refactor};
 
 impl Refactor {
     pub fn preprocess(&mut self) -> &mut Self {
-        let line_num = self.file().content.len();
-        self.file_mut().remove_dupl();
-        self.write_report(vec![format!(
-            "Lines reduced by preprocess (removing duplication): {}",
-            line_num - self.file().content.len()
-        )]);
         let tree = DirectoryTree::build_tree_from_file(&(self.file()));
         self.state.tree = tree;
         let (prev, (verbose, root, tree, file)) = self.get_borrows();
         if verbose {
             printv!(root, tree, file);
         }
+        let line_num = self.file().content.len();
+        self.file_mut().remove_dupl();
         if prev.violate {
             if self.state.lines_diff() > prev.state.unwrap().lines_diff() {
                 self.update(false);
@@ -24,6 +20,10 @@ impl Refactor {
         } else {
             self.update(false);
         }
+        self.write_report(vec![format!(
+            "Lines reduced by preprocess (removing duplication): {}",
+            line_num - self.file().content.len()
+        )]);
         self
     }
 }
